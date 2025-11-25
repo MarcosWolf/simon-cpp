9f127123a8923098cf078322b11f544050f9938e
Marcos Vinícios <viniciosramos.dev@gmail.com>
2025-11-25 02:10:53 -0300

    feat: Gerenciamento do jogo, ajustado intervalos de flash e pausa

--- simon-cpp/src/main.cpp
+++ simon-cpp/src/main.cpp
@@ -1,6 +1,15 @@
 #include "../include/Game.hpp"
+#include <iostream>
+
+using namespace std;
 
 int main() {
+	cout << "simon-cpp v0.1" << endl;
+	cout << "Developed by: Marcos Vinícios do Carmo Ramos" << endl;
+	cout << "Github: https://github.com/marcoswolf" << endl;
+	cout << "Game developed for study purposes. Hope you enjoy it!" << endl;
+	cout << "------------------------------------------------------------" << endl << endl;
+
 	Game game;
 	game.run();
 
